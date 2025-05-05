9993443926ff45870a2b138b33f01aa20f93068c
Ruslan Kazantsev <reteamboss@gmail.com>
2025-05-05 20:33:20 +0300

    Поправил ошибки

--- src/main/java/com/kaz/spring/mvc_hibernate_aop/entity/User.java
+++ src/main/java/com/kaz/spring/mvc_hibernate_aop/entity/User.java
@@ -18,12 +18,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String name;
-
     private String surname;
-
     private String department;
-
     private int salary;
 }
